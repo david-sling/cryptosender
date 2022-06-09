659f92cac5861f61a12964531e3bb30f09c4ff63
David S D <sddavid.johan@gmail.com>
2022-06-09 19:32:25 +0530

    init

---
+++ src/components/Header.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import classes from "styles/components/Header.module.scss";
+
+export const Header: FC = () => {
+  return <div className={classes.container}>Header</div>;
+};
